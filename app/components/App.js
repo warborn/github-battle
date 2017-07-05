fc11514e97d56c7b2f2a822937c7096819b1e319
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-07-05 17:11:05 -0500

    Add Nav component

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,20 @@
 var React   = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route  = ReactRouter.Route;
+
 var Popular = require('./Popular');
+var Nav = require('./Nav');
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className='container'>
-				<Popular />
-			</div>
+			<Router>
+				<div className='container'>
+					<Nav />
+					<Route path='/popular' component={Popular} />
+				</div>
+			</Router>
 		)
 	}
 }
